59a97f37edb1b2c34f1a5711a37b9b89a9e8f5e0
Evgeniya <speranskaya1991@mail.ru>
2023-10-10 19:40:30 +0500

    added task03

---
+++ task03/Program.cs
@@ -0,0 +1,71 @@
+﻿// Внутри класса Answer напишите методы CreateIncreasingMatrix, PrintArray, PrintListAvr и FindAverageInColumns.
+
+// Метод CreateIncreasingMatrix должен создавать матрицу заданной размерности, 
+//с каждым новым элементом увеличивающимся на определенное число. Метод принимает на вход три числа 
+//(n - количество строк матрицы, m - количество столбцов матрицы, k - число, на которое увеличивается 
+//каждый новый элемент) и возвращает матрицу, удовлетворяющую этим условиям.
+
+// Метод PrintArray должен выводить на экран сгенерированную методом CreateIncreasingMatrix матрицу.
+
+// Метод FindAverageInColumns принимает целочисленную матрицу типа int[,] и возвращает одномерный массив типа double. 
+//Этот метод вычисляет среднее значение чисел в каждом столбце матрицы и сохраняет результаты в виде списка.
+
+// Метод PrintListAvr принимает одномерный массив, возвращенный методом FindAverageInColumns
+// и выводит этот список на экран в формате "The averages in columns are: x.x0 x.x0 x.x0 ...", 
+//где x.x0 - это значения средних значений столбцов, округленные до двух знаков после запятой 
+//(в дробной части ВСЕГДА должно быть 2 числа через точку, см. формат вывода), разделенные знаком табуляции.
+
+
+using System;
+
+public class Answer {
+    public static void PrintArray (int [,] matrix)
+    {
+      // Введите свое решение ниже
+
+
+    }
+  
+    public static int[,] CreateIncreasingMatrix(int n, int m, int k)
+    {
+      // Введите свое решение ниже
+
+
+    }
+  
+    static void PrintListAvr (double [] list)
+    {
+      // Введите свое решение ниже
+
+
+    }
+
+    static double [] FindAverageInColumns (int [,] matrix)
+    { 
+      // Введите свое решение ниже
+
+
+    }
+
+
+    // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int n, m, k;
+
+        if (args.Length >= 3) {
+           n = int.Parse(args[0]);
+           m = int.Parse(args[1]);
+           k = int.Parse(args[2]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+           n = 3;
+           m = 4;
+           k = 2;
+        }
+
+        // Не удаляйте строки ниже
+        int[,] result = CreateIncreasingMatrix(n, m, k);
+        PrintArray(result);
+        PrintListAvr(FindAverageInColumns(result));
+    }
+}
