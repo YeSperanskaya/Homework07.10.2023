adfc3ff07ac18fb8521f9ee609a8397c3c5794b1
Evgeniya <speranskaya1991@mail.ru>
2023-10-10 22:17:06 +0500

    не получается решить 2 задачу

--- task02/Program.cs
+++ task02/Program.cs
@@ -26,6 +26,16 @@ public class Answer {
     public static void PrintArray (int [,] matrix)
     {
       // Введите свое решение ниже
+      for (int i = 0; i < matrix.GetLength(0); i++)
+      {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+          Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+      }
+
+
 
 
     }
@@ -33,22 +43,44 @@ public class Answer {
     public static int[,] CreateIncreasingMatrix(int n, int m, int k)
     {
       // Введите свое решение ниже
-
+      int [,] matr = new int[n, m];
+      int num = 1;
+      for (int i = 0; i < n; i++)
+      {
+        for (int j = 0; j < m; j++)
+        {
+          matr[i, j] = num;
+          num = num + k;
+        }
+      }
+      return matr;
 
     }
   
     public static int[] FindNumberByPosition (int [,] matrix, int rowPosition, int columnPosition)
     {  
       // Введите свое решение ниже
-
+      
+      if (rowPosition < matrix.GetLength(0) && columnPosition < matrix.GetLength(1)) 
+      {
+        int[] array = new int[2] {matrix[rowPosition, columnPosition], 0};
+        return array;        
+      }
+      int[] arrayNull = new int[0];
+      return arrayNull;
 
     }
 
     public static void PrintCheckIfError (int[] results, int X, int Y)
     {
       // Введите свое решение ниже
-
-
+      if (results[0] > 0) 
+      {
+        Console.WriteLine($"The number in [{X}, {Y}] is {results[0]}");
+      }
+      else 
+      {
+        Console.WriteLine("There is no such index");
         }
     }
 
@@ -64,11 +96,11 @@ public class Answer {
            y = int.Parse(args[4]);
         } else {
            // Здесь вы можете поменять значения для отправки кода на Выполнение
-           n = 3;
+           n = 6;
            m = 4;
            k = 2;
-           x = 2;
-           y = 3;
+           x = 8;
+           y = 8;
         }
 
         // Не удаляйте строки ниже
